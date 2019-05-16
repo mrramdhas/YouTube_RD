42165ff8487d29594611ca730821dde6f812e431
Ramdhas <ramdhasmails@gmail.com>
2019-05-16 13:20:56 +0530

    Getting response from server.

--- app/src/main/java/com/example/youtube_rd/MainActivity.kt
+++ app/src/main/java/com/example/youtube_rd/MainActivity.kt
@@ -8,6 +8,8 @@ import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.widget.LinearLayout
 import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.*
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,6 +21,26 @@ class MainActivity : AppCompatActivity() {
         recyclerView_main.layoutManager = LinearLayoutManager(this) as RecyclerView.LayoutManager?
         recyclerView_main.adapter = Main_Adapter()
 
+        fetchJson()
+
+    }
+
+    fun fetchJson () {
+        val urlString = "https://api.letsbuildthatapp.com/youtube/home_feed"
+        val request  = Request.Builder().url(urlString).build()
+
+        var client = OkHttpClient()
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                    println("failed")
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val response = response.body()?.string()
+                print(response)
+            }
+
+        })
 
     }
 }
